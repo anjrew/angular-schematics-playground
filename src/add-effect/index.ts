9e8b970e2a540d1121123bd8fc0d8fd3e2a5b26a
Andrew <earyzhe@gmail.com>
2020-12-04 15:03:59 +0100

    Got the Effects adding properly

--- src/add-effect/index.ts
+++ src/add-effect/index.ts
@@ -11,15 +11,14 @@ import { AddEffectSchema } from './schema';
 
 /* TYPESCRIPT */
 import * as ts from 'typescript';
-import * as fs from 'fs'
 import { getSourceNodes, insertImport } from '@schematics/angular/utility/ast-utils';
 import { InsertChange } from '@schematics/angular/utility/change';
+import chalk = require('chalk');
 
 const stringUtils = { classify, dasherize, capitalize, camelize }
 
 // You don't have to export the function as default. You can also have more than one rule factory per file.
 export function addEffect(options: AddEffectSchema): Rule {
-  console.log(`The option args are`, options)
 
   /* Correctly format the options path */
   options.path = options.path ? normalize(options.path) : '/';
@@ -34,38 +33,71 @@ export function addEffect(options: AddEffectSchema): Rule {
 
       let sourceFile: ts.SourceFile = ts.createSourceFile(options.path ?? '', content, ts.ScriptTarget.Latest, true)
 
+      // console.log(chalk.bgBlue(`Entire tree`))
       // showTree(sourceFile)
 
       const nodes = getSourceNodes(sourceFile)
 
-      const moduleEffectsArrayNode = nodes.find(n => n.kind === ts.SyntaxKind.Identifier && n.getChildCount() === 0 && n.getText().includes('ModuleEffects'))
+      const moduleEffectsArrayNode = nodes.find(n => {
 
-      if (moduleEffectsArrayNode) {
-        // console.log(moduleEffectsArrayNode)
+        const master = n.kind === ts.SyntaxKind.VariableDeclaration;
 
-        showTree(moduleEffectsArrayNode)
+        if (master) {
 
-        const arrayChange = new InsertChange(options.path ?? '', moduleEffectsArrayNode.pos, "WAAASAAPPP")
-        
-        const importChange = insertImport(sourceFile, options.path ?? '', 'TEST SYMBOL', 'Test file name')
+          const children = n.getChildren()
+          children.forEach(c => showTree(c))
 
-        const changes = [arrayChange, importChange]
-
-        const declarationRecorder = tree.beginUpdate(options.path ?? '',);
-        for (const change of changes) {
-          if (change instanceof InsertChange) {
-            declarationRecorder.insertLeft(change.pos, change.toAdd)
+          const isModuleEffectsArray = children.find(c => {
+            showTree(c)
+            return c.kind === ts.SyntaxKind.Identifier && c.getChildCount() === 0 && c.getText().includes('ModuleEffects')
           }
+          ) 
+  
+          return !!master && !!isModuleEffectsArray;
+
         }
 
-        tree.commitUpdate(declarationRecorder)
+      })
 
-      } else { 
+      if (moduleEffectsArrayNode) {
+        const children = moduleEffectsArrayNode.getChildren()
+
+        const arrayNode = children.find(n => n.kind === ts.SyntaxKind.ArrayLiteralExpression)
+
+        if (arrayNode) {
+
+          const filteredChildren = arrayNode.getChildren().filter(c => c.getText())
+          
+          const elementCountInArray = filteredChildren.length - 2;
+          console.log(`elementCountInArray`, elementCountInArray)
+
+          const textForArray = elementCountInArray !== 0 ? `,\n\t${classify(options.name)}` : `\n\t${classify(options.name)}` ;
+          
+          const arrayChange = new InsertChange(options.path ?? '', arrayNode.getChildren()[arrayNode.getChildren().length - 1].pos, textForArray)
+          
+          const importChange = insertImport(sourceFile, options.path ?? '', classify(options.name), options.path ?? '')
+          
+          const changes = [arrayChange, importChange]
+          
+          const declarationRecorder = tree.beginUpdate(options.path ?? '',);
+          for (const change of changes) {
+            if (change instanceof InsertChange) {
+              declarationRecorder.insertLeft(change.pos, change.toAdd)
+            }
+          }
+          
+          tree.commitUpdate(declarationRecorder)
+
+        } else { 
+          throw new SchematicsException(`No inner array was found`)
+        }
+
+      } else {
         throw new SchematicsException(`Cannot find module affects array Node in file`)
 
       }
 
-      
+
     } else {
       throw new SchematicsException(`Cannot find file at ${options.path}`)
     }
